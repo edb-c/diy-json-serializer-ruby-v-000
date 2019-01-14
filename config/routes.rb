ab049930ce6bbf137d84232f15c72b95d24bc370
Ethel Cofresi <cofresi.ethel@gmail.com>
2019-01-14 16:53:17 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
-  get 'posts/:id/post_data', to: 'posts#post_data'
+  get 'home', to: 'static#home'
+  resources :orders
+  resources :invoices
+  resources :products, only: [:index, :new, :create,:show]
+  get '/products/:id/description', to: 'products#description'
+  get '/products/:id/inventory', to: 'products#inventory'
+
+  get '/products/:id/data', to: 'products#data'
+
 end
